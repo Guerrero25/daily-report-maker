92e12f6e73920bed6c4864af72e40f41d7fa89ae
Omar Guerrero <guerrero9725@gmail.com>
2019-07-14 11:32:49 -0500

    feat: Added form for login

---
+++ src/components/Auth/Login/Form/index.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+/* Native Base */
+import { Form, Item, Picker, Icon, Label, Button, Text } from "native-base";
+/* Constants */
+import { colors } from "../../../../constants";
+
+type AuthLoginFormProps = {};
+
+function AuthLoginForm({  }: AuthLoginFormProps) {
+  const [selectedGitlabUsername, setSelectedGitlabUsername] = useState(null);
+
+  return (
+    <Form>
+      <Item style={styles.item} picker>
+        <Label>Gitlab username:</Label>
+        <Picker
+          mode="dropdown"
+          note={false}
+          iosIcon={<Icon name="arrow-down" />}
+          style={{ width: "100%" }}
+          placeholder="Select your username"
+          placeholderStyle={{ color: colors.primaryColor }}
+          placeholderIconColor={colors.primaryColor}
+          selectedValue={selectedGitlabUsername}
+          onValueChange={value => setSelectedGitlabUsername(value)}
+        >
+          <Picker.Item label="Omar Guerrero" value="@guerrero7" />
+          <Picker.Item label="Carlos Castillo" value="@cmaxhm" />
+          <Picker.Item label="Charly Cerdeño" value="@ccardenoc" />
+          <Picker.Item label="Ever  Cabarcas" value="@EverCabarcasM" />
+          <Picker.Item label="Jhonathan Cantillo" value="@jcantillog" />
+          <Picker.Item label="Leonardo Mira" value="@leonardozn" />
+          <Picker.Item label="Luis Morales" value="@luismoralesp" />
+          <Picker.Item label="Ivan Ospino" value="@Ivan_Ospino" />
+        </Picker>
+      </Item>
+      <Button style={styles.action}>
+        <Text>Log In</Text>
+      </Button>
+    </Form>
+  );
+}
+
+export default AuthLoginForm;
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginBottom: 40
+  },
+  action: {
+    width: "100%",
+    justifyContent: "center"
+  }
+});
--- src/screens/Auth/LogIn/index.tsx
+++ src/screens/Auth/LogIn/index.tsx
@@ -1,26 +1,22 @@
 import * as React from "react";
+import { StyleSheet } from "react-native";
 /* Native Base Components */
 import { Text, Button } from "native-base";
 /* UI Components */
 import Layout from "../../../components/UI/Layout";
-/* Service */
-import NavigationService from "../../../services/NavigationService";
+import AuthLoginForm from "../../../components/Auth/Login/Form";
 /* Assets */
 import E43Logo from "../../../assets/images/e43-logo.svg";
+import { colors } from "../../../constants";
 
 export interface ScreenLoginProps {}
 
 const ScreenLogin = () => {
   return (
     <Layout headerContent={<E43Logo width={200} height={60} />}>
-      <Text>Screen Login</Text>
+      <Text style={styles.title}>Login</Text>
 
-      <Button
-        style={{ alignSelf: "center" }}
-        onPress={() => NavigationService.navigate("App")}
-      >
-        <Text>Login</Text>
-      </Button>
+      <AuthLoginForm />
     </Layout>
   );
 };
@@ -30,3 +26,12 @@ ScreenLogin.navigationOptions = {
 };
 
 export default ScreenLogin;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 26,
+    color: colors.primaryColor,
+    marginBottom: 20,
+    fontWeight: "bold"
+  }
+});
