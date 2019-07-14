25885b14e8cd2e57a7d2c3f96603656cccbebd51
Omar Guerrero <guerrero9725@gmail.com>
2019-07-14 13:15:15 -0500

    feat: Added logout logic on home screen

--- src/screens/App/Home/index.tsx
+++ src/screens/App/Home/index.tsx
@@ -1,25 +1,33 @@
 import * as React from "react";
 import { TabBarIconProps } from "react-navigation";
+/* Redux Actions */
+import { changeGitlabUsername } from "../../../redux/actions/user";
 /* Native Base Components */
 import { Text, Button, Icon } from "native-base";
 /* UI Components */
 import Layout from "../../../components/UI/Layout";
 /* Services */
 import NavigationService from "../../../services/NavigationService";
+/* Utils */
+import { useActions } from "../../../utils/hooks";
 
 const Home = () => {
+  const changeCurrentUsernameAction = useActions(changeGitlabUsername);
+
   return (
     <Layout>
       <Text>Screen Home</Text>
 
-      <Button
-        style={{ alignSelf: "center" }}
-        onPress={() => NavigationService.navigate("Auth")}
-      >
+      <Button style={{ alignSelf: "center" }} onPress={handleLogout}>
         <Text>Go to Login Screen</Text>
       </Button>
     </Layout>
   );
+
+  function handleLogout() {
+    changeCurrentUsernameAction("");
+    NavigationService.navigate("Auth");
+  }
 };
 
 Home.navigationOptions = {
