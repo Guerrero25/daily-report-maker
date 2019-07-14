659a3bf6993abd3a13eaf991428f6881f4261015
Omar Guerrero <guerrero9725@gmail.com>
2019-07-14 12:59:41 -0500

    feat: Added validation for loading path

--- src/screens/Auth/Loading/index.tsx
+++ src/screens/Auth/Loading/index.tsx
@@ -8,16 +8,28 @@ import Layout from "../../../components/UI/Layout";
 import NavigationService from "../../../services/NavigationService";
 /* Assets */
 import E43Logo from "../../../assets/images/e43-logo.svg";
+import { useSelector } from "react-redux";
 
 const ScreenLoading = () => {
+  const currentUsername = useSelector(
+    (state: any) => state.user.currentUsername
+  );
+
   useEffect(() => {
     setTimeout(() => {
-      NavigationService.navigate("Auth");
+      if (currentUsername === "") {
+        NavigationService.navigate("Auth");
+      } else {
+        NavigationService.navigate("App");
+      }
     }, 1500);
   }, []);
 
   return (
-    <Layout showContent={false} headerContent={<E43Logo width={200} height={70} />}>
+    <Layout
+      showContent={false}
+      headerContent={<E43Logo width={200} height={70} />}
+    >
       <Spinner color="white" />
     </Layout>
   );
