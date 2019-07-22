70ef637db2db3a50455078c0f1991697fd6beb95
Omar Guerrero <guerrero9725@gmail.com>
2019-07-21 23:13:11 -0500

    refactor: Using button for copy and share action

--- src/components/App/Report/ResumeStatus/index.tsx
+++ src/components/App/Report/ResumeStatus/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import moment from "moment";
 /* Native Base Components */
-import { Card, CardItem, Text, Body, Icon, Right } from "native-base";
+import { Card, CardItem, Text, Body, Icon, Right, Button } from "native-base";
 /* Types */
 import { DailyReport } from "../../../../types";
 import { colors } from "../../../../constants";
@@ -32,22 +32,26 @@ const ResumeStatus = ({  }: LayoutProps) => {
           <Text>Did you already do your daily report?</Text>
         </Body>
         <Right style={{ flexDirection: "row", justifyContent: "flex-end" }}>
-          <Icon
-            type="FontAwesome5"
-            name="copy"
-            style={[
-              styles.actionIcon,
-              wasDailyReportMade ? null : styles.actionIconDisabled
-            ]}
-          />
-          <Icon
-            type="FontAwesome5"
-            name="share-alt"
-            style={[
-              styles.actionIcon,
-              wasDailyReportMade ? null : styles.actionIconDisabled
-            ]}
-          />
+          <Button transparent icon disabled={!wasDailyReportMade}>
+            <Icon
+              type="FontAwesome5"
+              name="copy"
+              style={[
+                styles.actionIcon,
+                wasDailyReportMade ? null : styles.actionIconDisabled
+              ]}
+            />
+          </Button>
+          <Button transparent icon disabled={!wasDailyReportMade}>
+            <Icon
+              type="FontAwesome5"
+              name="share-alt"
+              style={[
+                styles.actionIcon,
+                wasDailyReportMade ? null : styles.actionIconDisabled
+              ]}
+            />
+          </Button>
         </Right>
       </CardItem>
     </Card>
@@ -78,8 +82,8 @@ const styles = StyleSheet.create({
   },
   actionIcon: {
     color: colors.primaryColor,
-    marginLeft: 10,
-    fontSize: 20
+    marginLeft: 15,
+    fontSize: 30
   },
   actionIconDisabled: {
     color: colors.disabledColor
